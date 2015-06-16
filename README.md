0ce67d842a40248c8b78395bb3e1cd59c83920fa
Benjamin21st <bzhang7@outlook.com>
2015-06-16 14:59:29 -0400

    Added class inheritance to make code more modular

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ### Starting the game
 Open up the `index.html` file inside the game folder, and you will be able to play
  this simple yet addictive frogger game.
+ 
+### Playing the game
+To play the game, use the keys "up", "down", "left", "right" key on your keyboard to move 
+your character across the street, but be careful, if you get hit by a bug, your character 
+will be reset to starting position. 
--- js/app.js
+++ js/app.js
@@ -1,81 +1,91 @@
 "use strict";
 
-var Enemy = function() {
-    this.sprite = 'images/enemy-bug.png';
-
-    /* Bug has a length of 101, starts from its head */
-    this.x = -101;
-    /* Randomize y for each new bug created
-    * ignore "off-bound" issues?
-    * */
-    var newY = Math.floor(Math.random() * 10) * 30;
-    this.y = newY;
+var Character = function() {
+    this.x = 0;
+    this.y = 0;
+    this.sprite = '';
 };
 
-Enemy.prototype.update = function(dt) {
-    if (this.x >= 600) {
-        var index = allEnemies.indexOf(this);
-        allEnemies.splice(index, 1);
-    }
-    this.x += dt * 100;
-
+Character.prototype.update = function(dt) {
     return dt;
 };
 
-Enemy.prototype.render = function() {
+Character.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-var Player = function () {
-    this.sprite = 'images/char-cat-girl.png';
+var Enemy = function (sprite) {
+    this.prototype = new Character();
+    this.prototype.constructor = this;
+
+    this.sprite = sprite;
+    this.x = -101;
+    this.y = Math.floor(Math.random() * 10) * 30;
+
+    this.render = this.prototype.render;
+
+    this.update = function (dt) {
+        if (this.x >= 600) {
+            var index = allEnemies.indexOf(this);
+            allEnemies.splice(index, 1);
+        }
+        this.x += dt * 100;
+
+        return dt;
+    };
+};
+
+
+var Player = function(sprite) {
+    this.prototype = new Character();
+    this.prototype.constructor = this;
+
+    this.sprite = sprite;
     this.x = 0;
     this.y = 400;
-};
 
-Player.prototype.update = function (dt) {
-    var _this = this;
-    /*
-    * If the player encounters an enemy, restart the game
-    * */
-    allEnemies.forEach(function(el) {
-        /* instead of meeting at the exact coordinates,
-         *  meet at somewhere near the bug kills the player,
-         *  if player gets killed, reset game and bugs
+    this.render = this.prototype.render;
+
+    this.update = function () {
+        var _this = this;
+        /*
+         * If the player encounters an enemy, restart the game
          * */
-        if ((_this.x >= el.x - 50 && _this.x <= el.x + 50) && (_this.y >= el.y - 50 && _this.y <= el.y + 50)) {
-            _this.x = 0;
-            _this.y = 400;
-            allEnemies.pop();
+        allEnemies.forEach(function (el) {
+            /* instead of meeting at the exact coordinates,
+             *  meet at somewhere near the bug kills the player,
+             *  if player gets killed, reset game and bugs
+             * */
+            if ((_this.x >= el.x - 50 && _this.x <= el.x + 50) && (_this.y >= el.y - 50 && _this.y <= el.y + 50)) {
+                _this.x = 0;
+                _this.y = 400;
+                allEnemies.pop();
+            }
+        });
+    };
+
+    this.handleInput = function (key) {
+        if (key === 'up') {
+            this.y -= 20;
+        } else if (key === 'right') {
+            this.x += 20;
+        } else if (key === 'left') {
+            this.x -= 20;
+        } else if (key === 'down') {
+            this.y += 20;
         }
-    });
+    };
 };
 
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-Player.prototype.handleInput = function (key) {
-    console.log(key);
-    if (key === 'up') {
-        this.y -= 20;
-    } else if (key === 'right') {
-        this.x += 20;
-    } else if (key === 'left') {
-        this.x -= 20;
-    } else if (key === 'down') {
-        this.y += 20;
-    }
-};
-
-var player = new Player();
-
+var player = new Player('images/char-cat-girl.png');
 var allEnemies = [];
 
 /* A function to add new enemies to the game */
 function addBugsToGame() {
     setInterval(function() {
-        var newBug = new Enemy();
+        var newBug = new Enemy('images/enemy-bug.png');
         allEnemies.push(newBug);
     }, 1500);
 }
