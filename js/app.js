1e677c1704e7b916e2872a61182f669bb1ecda69
Benjamin21st <bzhang7@outlook.com>
2015-06-17 10:41:08 -0400

    Added boundaries; refacrtored code to be more object oriented

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var Character = function() {
-    this.x = 0;
-    this.y = 0;
-    this.sprite = '';
+var Character = function(x, y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
 };
 
 Character.prototype.update = function(dt) {
@@ -15,77 +15,137 @@ Character.prototype.render = function() {
 };
 
 
-var Enemy = function (sprite) {
-    this.prototype = new Character();
-    this.prototype.constructor = this;
+var Enemy = function (x, y, sprite) {
+    Character.call(this, x, y, sprite);
+};
+Enemy.prototype = new Character();
+Enemy.prototype.constructor = Enemy;
+Enemy.prototype.update = function(dt) {
+    if (this.x >= 600) {
+        var index = allEnemies.indexOf(this);
+        allEnemies.splice(index, 1);
+    }
+    this.x += dt * 100;
 
-    this.sprite = sprite;
-    this.x = -101;
-    this.y = Math.floor(Math.random() * 10) * 30;
-
-    this.render = this.prototype.render;
-
-    this.update = function (dt) {
-        if (this.x >= 600) {
-            var index = allEnemies.indexOf(this);
-            allEnemies.splice(index, 1);
-        }
-        this.x += dt * 100;
-
-        return dt;
-    };
+    return dt;
 };
 
 
-var Player = function(sprite) {
-    this.prototype = new Character();
-    this.prototype.constructor = this;
-
-    this.sprite = sprite;
-    this.x = 0;
-    this.y = 400;
-
-    this.render = this.prototype.render;
-
-    this.update = function () {
-        var _this = this;
-        /*
-         * If the player encounters an enemy, restart the game
+var Player = function (x, y, sprite) {
+    Character.call(this, x, y, sprite);
+};
+Player.prototype = new Character();
+Player.prototype.constructor = Player;
+Player.prototype.update = function () {
+    var _this = this;
+    /*
+     * If the player encounters an enemy, restart the game
+     * */
+    allEnemies.forEach(function (el) {
+        /* instead of meeting at the exact coordinates,
+         *  meet at somewhere near the bug kills the player,
+         *  if player gets killed, reset player position as
+         *  is shown in the demo
          * */
-        allEnemies.forEach(function (el) {
-            /* instead of meeting at the exact coordinates,
-             *  meet at somewhere near the bug kills the player,
-             *  if player gets killed, reset game and bugs
-             * */
-            if ((_this.x >= el.x - 50 && _this.x <= el.x + 50) && (_this.y >= el.y - 50 && _this.y <= el.y + 50)) {
-                _this.x = 0;
-                _this.y = 400;
-                allEnemies.pop();
-            }
-        });
-    };
-
-    this.handleInput = function (key) {
-        if (key === 'up') {
-            this.y -= 20;
-        } else if (key === 'right') {
-            this.x += 20;
-        } else if (key === 'left') {
-            this.x -= 20;
-        } else if (key === 'down') {
-            this.y += 20;
+        if ((_this.x >= el.x - 50 && _this.x <= el.x + 50) && (_this.y >= el.y - 50 && _this.y <= el.y + 50)) {
+            _this.x = 0;
+            _this.y = 400;
+            //allEnemies.pop();
         }
-    };
+    });
+    /*
+     * If the player hits the water, restart the game
+     * */
+    if (this.y <= 0) {
+        this.x = 0;
+        this.y = 400;
+    }
+};
+Player.prototype.handleInput = function (key) {
+    if (key === 'up') {
+        this.y -= 20;
+    } else if (key === 'right' && this.x <= 380) {
+        this.x += 20;
+    } else if (key === 'left' && this.x >= 20) {
+        this.x -= 20;
+    } else if (key === 'down' && this.y <= 400) {
+        this.y += 20;
+    }
 };
 
+//var Player = function(sprite) {
+//    this.prototype = new Character();
+//    this.prototype.constructor = this;
+//
+//    this.sprite = sprite;
+//    this.x = 0;
+//    this.y = 400;
+//
+//    this.render = this.prototype.render;
+//
+//    this.update = function () {
+//        var _this = this;
+//        /*
+//         * If the player encounters an enemy, restart the game
+//         * */
+//        allEnemies.forEach(function (el) {
+//            /* instead of meeting at the exact coordinates,
+//             *  meet at somewhere near the bug kills the player,
+//             *  if player gets killed, reset game and bugs
+//             * */
+//            if ((_this.x >= el.x - 50 && _this.x <= el.x + 50) && (_this.y >= el.y - 50 && _this.y <= el.y + 50)) {
+//                _this.x = 0;
+//                _this.y = 400;
+//                //allEnemies.pop();
+//            }
+//        });
+//        /*
+//        * If the player hits the water, restart the game
+//        * */
+//        if (this.y == 0) {
+//            this.x = 0;
+//            this.y = 400;
+//        }
+//    };
+//
+//    this.handleInput = function (key) {
+//        if (key === 'up') {
+//            this.y -= 20;
+//        } else if (key === 'right' && this.x <= 380) {
+//            this.x += 20;
+//        } else if (key === 'left' && this.x >= 20) {
+//            this.x -= 20;
+//        } else if (key === 'down' && this.y <= 400) {
+//            this.y += 20;
+//        }
+//    };
+//};
 
-var player = new Player('images/char-cat-girl.png');
+var playerX = 0,
+    playerY = 400,
+    playerSprite = 'images/char-cat-girl.png',
+    player;
+
+player = new Player(playerX, playerY, playerSprite);
+
 var allEnemies = [];
 
 /* A function to add new enemies to the game */
 function addBugsToGame() {
     setInterval(function() {
-        var newBug = new Enemy('images/enemy-bug.png');
+        /*
+        * Rnadomize new bug's y position here
+        * instead of messing around with prototype
+        * */
+        var bugX,
+            bugY,
+            bugSprite,
+            newBug;
+        bugX = -101;
+        bugY = Math.floor(Math.random() * 10) * 30;
+        bugSprite = 'images/enemy-bug.png';
+        newBug = new Enemy(bugX, bugY, bugSprite);
+
         allEnemies.push(newBug);
     }, 1500);
 }
